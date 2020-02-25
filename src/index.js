550072c9041ddc48b43ba206a1b040ba721a43c6
mmktomato <mkt.motohashi@gmail.com>
2020-02-26 00:14:01 +0900

    discover package.json (#1)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const webExt = require("web-ext").default;
 const process = require("process");
+const path = require("path");
+const fsPromise = require("fs").promises;
 
 let runner = null;
+
+const getConfig = async () => {
+  // TODO: ~/.web-ext-config.js
+  // TODO: path.resolve(process.cwd(), "web-ext-config.js")
+
+  const packageJsonPath = path.resolve(process.cwd(), "package.json");
+  let obj = null;
+
+  try {
+    const content = await fsPromise.readFile(packageJsonPath, { encoding: "utf8" });
+    obj = JSON.parse(content);
+  } catch (e) {
+    return null;
+  }
+
+  return (obj && obj.webExt && obj.webExt.run) ? obj.webExt.run : null;
+};
 
 module.exports = bundler => {
   if (!bundler.options.watch) {
@@ -14,10 +33,15 @@ module.exports = bundler => {
       return;
     }
 
-    runner = await webExt.cmd.run({
-      noReload: true,
-      sourceDir: process.cwd(),
-    }, {
+    let config = await getConfig() || {};
+    if (!config.noReload) {
+      config.noReload = true;
+    }
+    if (!config.sourceDir) {
+      config.sourceDir = process.cwd();
+    }
+
+    runner = await webExt.cmd.run(config, {
       shouldExitProgram: true,
     });
 
